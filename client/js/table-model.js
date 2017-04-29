b6d35f20dfb9bea627202b7fb7cdded9373b65b8
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-04-28 23:13:40 -0400

    Another bad commit

--- client/js/table-model.js
+++ client/js/table-model.js
@@ -3,7 +3,6 @@ class TableModel {
     this.numCols = numCols;
     this.numRows = numRows;
     this.data = {};
-    this.sums = new Array(numCols);
   }
 
   _getCellId(location) {
@@ -18,22 +17,6 @@ class TableModel {
     this.data[this._getCellId(location)] = value;
   }
 
-  updateSum(col) {
-    let array = [];
-    let sum = 0;
-
-    // compute sum filtering undefined values and strings, etc.
-    for (let i = 0; i < this.numRows; i++) {
-      const location = { col: col, row: i };
-      let cellData = this.getValue(location);
-      array.push(cellData);
-    }
-
-    console.log(array);
-
-    this.sums[col] = sum;
-    return sum;
-  }
 }
 
 module.exports = TableModel;
